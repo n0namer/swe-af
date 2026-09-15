0bdcd4a317cdd096458c3d79e37ac0b876c821e9
Никита Гамолин <nik.gam@gmail.com>
2026-09-15 19:41:11 +0300

    sourceloop: replay resume input onto dev

--- go/internal/issue/build.go
+++ go/internal/issue/build.go
@@ -55,6 +55,7 @@ type implementInput struct {
 	Issue             map[string]any `json:"issue"`
 	RepoPath          string         `json:"repo_path"`
 	BaseBranch        string         `json:"base_branch"`
+	ResumeBuildID     string         `json:"resume_build_id"`
 	ArtifactsDir      string         `json:"artifacts_dir"`
 	AdditionalContext string         `json:"additional_context"`
 	Config            map[string]any `json:"config"`
